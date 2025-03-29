a84b909c9d7576b01b451a8ddc7b3f1c05f1ae6b
Jose Miguel <ramirez074jose@gmail.com>
2025-03-28 20:26:13 -0600

    Agragado de proyectos

--- src/Componets/Projects/Projects.jsx
+++ src/Componets/Projects/Projects.jsx
@@ -6,6 +6,11 @@ import ticosta from '../../assets/TicostaWeb.png';
 import lumina from '../../assets/LuminaApp.png';
 import alegria from '../../assets/AlegriaWeb.png';
 
+import jornadas from '../../assets/jornadas.jpg';
+import game from '../../assets/GameSpace.png';
+import ticostaDesign from '../../assets/TicostaWeb.png';
+import Mypreset from '../../assets/MyPreset.png';
+
 import ProjectCard from '../../common/ProjectCard'
 
 function Projects() {
@@ -29,6 +34,22 @@ function Projects() {
                 <ProjectCard src={alegria} link="https://github.com/JoseTruko/RecipeDepot.git"
                 h3="Alegría al cocinar"
                 p="Web de recetas / Vue.js"/>
+
+            </div>
+            <hr />
+            <div className={styles.projectsContainer}>
+
+                <ProjectCard src={jornadas} link=""
+                h3="Jornadas de Investigación"
+                p="Identidad gráfica UCR / Adobe Suite"/>
+
+                <ProjectCard src={game} link="https://www.figma.com/design/jLsRtvPAJJCjm25YBuJBOO/VideoGames-of-2023?node-id=0-1"
+                h3="Game Space"
+                p="Diseño web / Figma"/>
+
+                <ProjectCard src={Mypreset} link="https://www.figma.com/file/AJGyjVX9Ntuob1uZPS7rXv/My-Preset-App?type=design&node-id=0%3A1&mode=design&t=btlDa77EsF590UQi-1"
+                h3="My preset"
+                p="Diseño de app / Figma"/>
 
             </div>
         </section>
